a28175abdd2646205698de7dd64f32ad10c9882c
Beksultan <albinblack7@gmail.com>
2022-05-17 17:00:41 +0300

    Initial Commit

---
+++ ColorizedSwiftUIApp/ColorizedSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIAppApp.swift
+//  ColorizedSwiftUIApp
+//
+//  Created by Masaie on 17/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
